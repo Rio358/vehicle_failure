2bfe7195cbb2adc3ccda6fc1beac3faa7fc1817b
Rio358 <56245429+Rio358@users.noreply.github.com>
2021-06-19 18:50:11 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,7 @@ fx_version 'bodacious'
 
 game 'gta5'
 
-shared_script '@es_extended/imports.lua'
-
 client_scripts {
 	"config.lua",
 	"client.lua"
-}
-
-server_scripts {
-	"config.lua",
-	"server.lua"
 }
