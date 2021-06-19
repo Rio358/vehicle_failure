064664813e2c5d3520cf2909e408837533def05f
Rio358 <56245429+Rio358@users.noreply.github.com>
2021-06-19 18:35:02 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -31,8 +31,6 @@ math.randomseed(GetGameTimer());
 local tireBurstMaxNumber = cfg.randomTireBurstInterval * 1200; 												-- the tire burst lottery runs roughly 1200 times per minute
 if cfg.randomTireBurstInterval ~= 0 then tireBurstLuckyNumber = math.random(tireBurstMaxNumber) end			-- If we hit this number again randomly, a tire will burst.
 
-ESX = exports['es_extended']:getSharedObject()
-
 local function isPedDrivingAVehicle()
 	local ped = PlayerPedId()
 	vehicle = GetVehiclePedIsIn(ped, false)
